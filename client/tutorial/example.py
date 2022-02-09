5f22bddcc774f142ccd4c96b88e3d71cdfa35a69
JoFrost <mebessaa@student.42.fr>
2022-02-09 11:07:46 -0600

    distilbert example

--- client/tutorial/example.py
+++ client/tutorial/example.py
@@ -1,23 +1,21 @@
 from blindai.client import BlindAiClient
 from PIL import Image
 import numpy as np
-from transformers import AutoTokenizer
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
 checkpoint = "distilbert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
 # Create the connection
 client = BlindAiClient()
-client.connect_server(
-    "localhost", policy="policy.toml", certificate="host_server.pem", simulation=True
-)
+client.connect_server("localhost", simulation=True)
 
 # Upload the model to the server
-response = client.upload_model(model="./final_confidential_transformers_classifier.onnx", shape=(1, 20), datum=client.ModelDatumType.I64)
+response = client.upload_model(model="./distilbert-base-uncased.onnx", shape=(1, 8), datum=client.ModelDatumType.I64)
 
 if response.ok:
     print("Model loaded")
-    tokenized = tokenizer("I love potatoes", padding = "max_length", max_length = 20)["input_ids"]
+    tokenized = tokenizer("Hello, my dog is cute", padding = "max_length", max_length = 8)["input_ids"]
     res = client.run_model(tokenized)
     print(res.output)
 
