51c43c524dd0eada6993ffee8d5033122f65a699
JoFrost <mebessaa@student.42.fr>
2022-02-10 05:21:34 -0600

    added license

--- client/blindai/utils/utils.py
+++ client/blindai/utils/utils.py
@@ -1,13 +1,23 @@
+# Copyright 2022 Mithril Security. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import re
 import grpc
 import cryptography.x509
 from cryptography.hazmat.primitives.serialization import Encoding
 
-
-
 CHUNK_SIZE = 32 * 1024  # 32kb
-FLOAT_NB_ELEM = 16
-
 
 def strip_https(url: str):
     return re.sub(r"^https:\/\/", "", url)
@@ -22,14 +32,6 @@ def create_byte_chunk(data):
             data[sent_bytes : sent_bytes + min(CHUNK_SIZE, len(data) - sent_bytes)]
         )
         sent_bytes += min(CHUNK_SIZE, len(data) - sent_bytes)
-
-
-def create_float_chunk(data):
-    sent = 0
-    while sent < len(data):
-        yield data[sent : sent + min(FLOAT_NB_ELEM, len(data) - sent)]
-        sent += min(FLOAT_NB_ELEM, len(data) - sent)
-
 
 def check_rpc_exception(rpc_error):
     if rpc_error.code() == grpc.StatusCode.CANCELLED:
