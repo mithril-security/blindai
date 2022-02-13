22852ea3a1abd896c27badc76f026a4f42d43fac
cchudant <cchudant@student.42.fr>
2022-02-13 13:12:04 +0100

    reproducible builds using docker

--- server/docker/hardware-start.sh
+++ server/docker/hardware-start.sh
@@ -2,6 +2,6 @@
 /opt/intel/sgx-aesm-service/aesm/aesm_service --no-daemon &
 cd /opt/intel/sgx-dcap-pccs
 sed -i '/ApiKey/c\   \"ApiKey\" : \"'$1'\",' config/default.json 
-npm start pm2 & 
-cd /root/
+pm2 & 
+cd /root/bin
 ./app
