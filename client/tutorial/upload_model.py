38bbabe7d7f6173660674fd641f3d814176e1f58
CerineBnsd <hc_bensadi@esi.dz>
2022-02-21 00:04:59 -0600

    Remove unnecessary try and except blocks

--- client/tutorial/upload_model.py
+++ client/tutorial/upload_model.py
@@ -1,8 +1,8 @@
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
-import torch
+#from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+#import torch
 from blindai.client import BlindAiClient, ModelDatumType
 
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+"""tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
 
 sentence = "Hello, my dog is cute"
@@ -17,9 +17,9 @@ torch.onnx.export(model,
                   input_names = ['input'],
                   output_names = ['output'],
                   dynamic_axes={'input' : {0 : 'batch_size'},'output' : {0 : 'batch_size'}})
-
+"""
 client = BlindAiClient()
-client.connect_server("localhost", simulation=True)
+client.connect_server("localhost", certificate="policy.toml", policy="policy.toml",simulation=False)
 
 #Upload the model to the server
 response = client.upload_model(model="./distilbert-base-uncased.onnx", shape=(1, 8), dtype=ModelDatumType.I64)
