5150dd8aee1113ecf0514f86c4f01993ece47ee6
cchudant <cchudant@student.42.fr>
2022-03-21 14:40:25 +0100

    ci: Build the client and server and run the tests

--- client/setup.py
+++ client/setup.py
@@ -47,6 +47,8 @@ class CMakeBuild(build_ext):
             f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",
+            f"-DBUILD_TESTS=false",
+            f"-DBUILD_ATTESTATION_APP=false",
         ]
 
         build_args = []
