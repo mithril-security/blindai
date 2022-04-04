a8243ebe278e3b74e976efe08a6aac8e05047047
JoFrost <mebessaa@student.42.fr>
2022-04-04 06:34:56 -0500

    fix clippy warning

--- server/blindai_app/src/main.rs
+++ server/blindai_app/src/main.rs
@@ -75,8 +75,9 @@ impl untrusted_local_app_server::UntrustedLocalApp for State {
 
 fn fill_blank_and_print(content: &str, size: usize)
 {
-    let trail: String = repeat("#").take((size - 2 - content.len()) / 2).collect::<String>();
-    let trail2: String = repeat("#").take(((size - 2 - content.len()) as f32 / 2.0).ceil() as usize).collect::<String>();
+    let trail_char = "#";
+    let trail: String = trail_char.repeat((size - 2 - content.len()) / 2);
+    let trail2: String = trail_char.repeat(((size - 2 - content.len()) as f32 / 2.0).ceil() as usize);
     println!("{} {} {}", trail, content, trail2);
 }
 
