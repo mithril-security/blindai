ba3fefda417097b207af5e4b994e25e1023fe086
ivhamponsem@gmail.com <ivh@IVHs-iMac.local>
2022-05-13 08:25:25 -0700

    Update: adds comments to Darwin post build file

--- client/scripts/postExtensionBuildDarwin.sh
+++ client/scripts/postExtensionBuildDarwin.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# install_name_tool is used on OSX
+# A replacement for realpath on Linux
 __realpath(){
     local path=$1
     dir=$(dirname $path)
@@ -11,8 +11,13 @@ __realpath(){
         echo $full_dir    
     fi
 }
+
+# install_name_tool is used on OSX
+
 QUOTE_LIB=$(find . -name "_quote_verification*.so")
 BASE_DIR=$(__realpath $(basename ${QUOTE_LIB}))
 cd -
+
+# install_name_tool replaces patchelf on Linux
 install_name_tool -change libverify.dylib "$BASE_DIR/blindai/lib/libverify.dylib" ${QUOTE_LIB}
 install_name_tool -change @rpath/libQuoteVerification.dylib "$BASE_DIR/blindai/lib/libQuoteVerification.dylib" ${QUOTE_LIB}
