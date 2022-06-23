4cacf25ba9660a6e78f6b5b348b415581f0f6243
Matthias Brunel <56654600+mbrunel@users.noreply.github.com>
2022-06-23 15:33:31 +0200

    change commit author to github action bot

--- .github/scripts/update_gitbook_public.sh
+++ .github/scripts/update_gitbook_public.sh
@@ -1,14 +1,11 @@
 set -e
 set -x
 
-git config --global user.email "matthias.brunel@mithrilsecurity.io"
-git config --global user.name "mbrunel"
-
 git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/mithril-security/gitbook_public.git" "./gitbook"
 
 cp "docs/client.py.md" "gitbook/resources/client-api-reference/client-interface-$VERSION.md"
 cd gitbook
 python .github/scripts/update_summary.py $VERSION
 git add .
-git commit -m "Automatic update due to new released version of blindai client"
+git commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m "Automatic update due to new released version of blindai client"
 git push
