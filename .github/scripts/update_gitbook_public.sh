5de5198614dd800a0d769bac3ffaa5de8a14d26f
Matthias Brunel <56654600+mbrunel@users.noreply.github.com>
2022-06-23 16:37:33 +0200

    Fixing author of gitbook_public commit

--- .github/scripts/update_gitbook_public.sh
+++ .github/scripts/update_gitbook_public.sh
@@ -6,6 +6,9 @@ git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GIT
 cp "docs/client.py.md" "gitbook/resources/client-api-reference/client-interface-$VERSION.md"
 cd gitbook
 python .github/scripts/update_summary.py $VERSION
+
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
 git add .
-git commit --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" -m "Automatic update due to new released version of blindai client"
+git commit -m "Automatic update due to new released version of blindai client"
 git push
