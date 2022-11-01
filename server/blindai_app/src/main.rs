212e675096b23291a672fe55d80ff61ea5d06026
mebessaa <mebessaa@student.42.fr>
2022-11-01 15:56:25 +0100

    blindai_app/main: simplify trail formula

--- server/blindai_app/src/main.rs
+++ server/blindai_app/src/main.rs
@@ -78,7 +78,7 @@ fn fill_blank_and_print(content: &str, size: usize) {
     let trail_char = "#";
     let trail: String = trail_char.repeat((size - 2 - content.len()) / 2);
     let trail2: String =
-        trail_char.repeat(((size - 2 - content.len()) as f32 / 2.0).ceil() as usize);
+        trail_char.repeat((size - 1 - content.len()) / 2);
     println!("{} {} {}", trail, content, trail2);
 }
 
