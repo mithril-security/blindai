e53f5d9d314652693167980d0fa448b8d54f7412
JoFrost <mebessaa@student.42.fr>
2023-03-15 16:21:25 +0000

    feat: pccs key can be specified when running the docker image

--- .devcontainer/hw-start.sh
+++ .devcontainer/hw-start.sh
@@ -3,6 +3,7 @@ set -e
 
 # /opt/intel/sgx-aesm-service/aesm/aesm_service --no-daemon &
 cd /opt/intel/sgx-dcap-pccs
+sed -i '/ApiKey/c\   \"ApiKey\" : \"'$1'\",' config/default.json 
 npm start pm2 &
 cd /root
 ./runner blindai_server.sgxs
