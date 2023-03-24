75ea1d64645fda129b3deaaff603e1013c86e996
JoFrost <mebessaa@student.42.fr>
2023-03-24 00:03:30 +0000

    fix: api_test file will now upload the model

--- tests/audio/api_test.py
+++ tests/audio/api_test.py
@@ -9,10 +9,12 @@ if os.environ.get("BLINDAI_SIMULATION_MODE") == "true":
 else:
     connection = core.connect(addr="localhost", hazmat_http_on_unattested_port=True)
 
+response = connection.upload_model(model="whisper.onnx")
+
 # Download audio file
 output = os.path.join(os.path.dirname(__file__), "taunt.wav")
 os.system(f"wget https://www2.cs.uic.edu/~i101/SoundFiles/taunt.wav -O {output}")
 
 # Transcribe audio file
-transcript = api.Audio.transcribe(file=output, connection=connection)
+transcript = api.Audio.transcribe(file=output)
 assert transcript == " Now go away, or I shall taunt you a second timer!"
