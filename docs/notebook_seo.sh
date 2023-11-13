178424e9945280bc1530e585a843c9ba8354869f
lyie28 <laurayie93@gmail.com>
2023-11-13 13:46:30 +0100

    fix

---
+++ docs/notebook_seo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e  # Exit if any command fails
+
+sed -i '/<head>/a <meta name="description" content="Quick Tour of BlindAI API: Open-source Python library for private AI model access and Whisper model audio transcription.">' site/docs/getting-started/quick-tour/index.html
