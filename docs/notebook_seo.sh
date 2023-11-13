5821e60f5b40aa2064b32fe23566b708d5b3afa3
lyie28 <laurayie93@gmail.com>
2023-11-13 14:01:28 +0100

    testing

--- docs/notebook_seo.sh
+++ docs/notebook_seo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e  # Exit if any command fails
 
-sed -i '/<head>/a <meta name="description" content="Quick Tour of BlindAI API: Open-source Python library for private AI model access and Whisper model audio transcription.">' site/docs/getting-started/quick-tour/index.html
+sed -i '/<head>/a <meta name="description" content="Quick Tour of BlindAI API: Open-source Python library for private AI model access and Whisper model audio transcription.">' $READTHEDOCS_OUTPUT/html/docs/getting-started/quick-tour/index.html
